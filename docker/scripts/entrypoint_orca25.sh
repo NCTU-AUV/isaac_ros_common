86dadd4767158a1bf987f516ed9efdc4c58b2279
ycchiu226 <ycchiu226@gmail.com>
2025-09-18 17:51:34 +0800

    feat: source the mesh path of gazebo fortress in entrypoint script

--- docker/scripts/entrypoint_orca25.sh
+++ docker/scripts/entrypoint_orca25.sh
@@ -1,2 +1,3 @@
 source install/setup.bash
-export GZ_SIM_RESOURCE_PATH=$GZ_SIM_RESOURCE_PATH:$ISAAC_ROS_WS/src/sauvc_sim/models/
+export GZ_SIM_RESOURCE_PATH=$ISAAC_ROS_WS/src/sauvc_sim/models/:$ISAAC_ROS_WS/src/sauvc_sim/meshes/
+
